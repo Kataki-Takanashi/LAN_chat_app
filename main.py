b219169b256030a36992ce6b03a39be857313b5f
Kataki <alibugrahim06@gmail.com>
2022-09-17 15:57:14 -0400

    Testing Revised Server Searching

--- main.py
+++ main.py
@@ -44,7 +44,13 @@ import sys
 import threading
 import os
 import logging
+import time
+
 import server
+import serverFinder
+from tqdm import tqdm
+
+#from test1 import device
 
 # Logging
 
@@ -60,6 +66,7 @@ file.setFormatter(formater)
 logger.addHandler(stream)
 logger.addHandler(file)
 
+test = serverFinder.test()
 sever = server.Server()
 nickname = input("Choose your nickname : ").strip()
 while not nickname:
@@ -68,29 +75,90 @@ my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 devices = []
 print("Looking for open server. . .")
-for device in os.popen('arp -a'):
-    devices.append(device.split()[1])  # .strip("()"))
+for devise in os.popen('arp -a'):
+    devices.append(devise.split()[1].strip("()"))  # .strip("()"))
 # print(devices)
+#filter
+
+for i in devices:
+    delete = False
+    if not i.startswith('192'):
+        delete = True
+    if i.startswith('224'):
+        delete = True
+    if delete:
+        devices.remove(i)
+
+# Priority
+priority = []
+for i in devices:
+    if i.startswith('192.168.4'):
+        priority.append(i)
+        devices.remove(i)
+devices = priority #+ devices
+
 
 make_server = True
-# host = "192.168.4.24"  # "127.0.1.1"1`
+host = "192.168.4.24"  # "127.0.1.1"1`
 port = 8000
-for device in devices:
-    try:
-        my_socket.connect((device, port))
-        make_server = False
-        print(f"Connected to {device}")
-        break
-    except socket.error as error:
-        logger.error(f'No route to {device}')
-        # print(error)
+itterDevice = 0
 
+result = serverFinder.test.threadTeast(serverFinder.test(), devices)
+if result != False:
+    my_socket.connect((result, port))
+else:
+    #logger.info(f"No connection to {device}")
+    pass
+
+# def start_search_thread(devices):
+#     for i in devices:
+#         search_thread = threading.Thread(
+#             target=serch_thread,
+#             args=(i,),  # the list of argument for the function
+#             daemon=True
+#         )
+#
+#         search_thread.start()
+#
+# connectable_device = 'null' #placeholder
+# def serch_thread(device):
+#     try:
+#         my_socket.connect((device, port))
+#         make_server = False
+#         connectable_device = device
+#         print(f"Connected to {device}")
+#         return True
+#     except socket.error as error:
+#         logger.error(f'{error} on {device}')
+#         return False
+#     finally:
+#         my_socket.close()
+#         return
+#
+#
+# start_search_thread(devices)
+    # try:
+    #     my_socket.connect((device, port))
+    #     make_server = False
+    #     print(f"Connected to {device}")
+    #     break
+    # except socket.error as error:
+    #     logger.error(f'{error} on {device}')
+    #     # print(error)
+time.sleep(2.5)
 if make_server:
+    print("No open server detected")
+    print("Starting server")
     start_server = threading.Thread(target=sever.accept_loop)
     # server.accept_loop()
     start_server.start()
+    time.sleep(1)
     my_socket.connect((socket.gethostbyname(socket.gethostname()), port))
+    #my_socket.connect((host, port))
     print(f"Connected to {socket.gethostbyname(socket.gethostname())}")
+    #print(f"Connected to {host}")
+else:
+    my_socket.connect((connectable_device, port))
 
 
 def thread_sending():
